6b685f47503ddb1e74ec169773a2c12b91300228
ShahidBaltistani <47021740+ShahidBaltistani@users.noreply.github.com>
2019-11-25 11:57:03 +0500

    CockpitComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CockpitComponent
   ],
   imports: [
     BrowserModule,
